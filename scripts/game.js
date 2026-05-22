b61ce70184777304986442150dc09e10bcbaae0b
Karenpo <karenpo@ya.ru>
2026-05-22 07:14:48 +0300

    Added computer and user choice type of code

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,50 @@
-console.log("Hello World")
+/* console.log("Hello World")
+для проверки, что все работает */ 
+
+function getComputerChoice() {
+  const randomNumber = Math.random();
+  
+  if (randomNumber < 1 / 3) { // здесь сначала я записал 0.33. теперь JS сам разделит на равные части
+    return "Scissors";
+  } else if (randomNumber < 2 / 3) {
+    return "Rock";
+  } else {
+    return "Paper"; 
+  }
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+  let choice = prompt("Choose Rock, Scissors or Paper");
+  return choice.toLowerCase(); //изначально написал просто choice, но надо привести к единому регистру, т.к. пользователь может написать и с большой буквы
+}
+
+console.log(getHumanChoice());
+
+let humanScore = 0; 
+let getComputerScore = 0;
+
+/* вариант этого кода с массивом. 
+function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
+*/
+
+ 
+/*function favoriteAnimal(animal) {
+  return animal + " is my favorite animal"
+}
+
+console.log(favoriteAnimal ('Goat'));
+
+function sum(param1, param2) {
+  return param1 + param2; 
+}
+
+console.log (sum(1, 2));
+*/
